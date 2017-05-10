03c81e9a2f1313efdd4c440c25df86f1e71cd081
divramod <divramod@gmail.de>
2017-05-10 08:15:15 +0200

    automatic sync at 2017.05.10 08:15:15 by divramod  DELETED:  MODIFIED: test/rsync.sh  ADDED:

--- test/rsync.sh
+++ test/rsync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #sudo rsync -avq /home/mod /media/mod/HD11/backups-home/20170509_x121e --delete
-sudo rsync -av --exclude-from='/home/mod/sync/exclude.txt' /home/mod /media/mod/HD11/backups-home/20170509_x121e --delete
+sudo rsync -av --exclude-from='/home/mod/.sync/exclude.txt' /home/mod /media/mod/HD11/backups-home/20170509_x121e --delete
 optspec=":hv-:"
 while getopts "$optspec" optchar; do
     case "${optchar}" in
