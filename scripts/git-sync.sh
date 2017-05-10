23c7bbcc8a951858dfbfe81eed7116f32ea2b39e
divramod <divramod@gmail.de>
2017-05-10 08:15:13 +0200

    automatic sync at 2017.05.10 08:15:13 by divramod  DELETED:  MODIFIED: scripts/git-sync.sh  ADDED: test/1.txt

--- scripts/git-sync.sh
+++ scripts/git-sync.sh
@@ -11,47 +11,49 @@ function syncGit() {
     echo &&
       echo "SYNCING $PWD" &&
       echo "=======================================================================" &&
-      GIT_DELETED=$(git ls-files --deleted --exclude-standard) &&
-      GIT_MODIFIED=$(git ls-files --modified --exclude-standard) &&
-      GIT_OTHERS=$(git ls-files --others --exclude-standard) &&
-      rm -f /tmp/commit_msg.txt &&
-      touch /tmp/commit_msg.txt &&
-      echo 'automatic sync at '$(date +%Y.%m.%d)' '$(date +%H:%M:%S)' by '$(git config user.name) >> /tmp/commit_msg.txt &&
-      echo ' DELETED: '$GIT_DELETED >> /tmp/commit_msg.txt &&
-      echo ' MODIFIED: '$GIT_MODIFIED >> /tmp/commit_msg.txt &&
-      echo ' ADDED: '$GIT_OTHERS >> /tmp/commit_msg.txt &&
-      git add -A &&
-      git commit -F /tmp/commit_msg.txt &&
-      rm /tmp/commit_msg.txt &&
+      if [[ ! -z $(git status -s) ]]; then
+        GIT_DELETED=$(git ls-files --deleted --exclude-standard) &&
+          GIT_MODIFIED=$(git ls-files --modified --exclude-standard) &&
+          GIT_OTHERS=$(git ls-files --others --exclude-standard) &&
+          rm -f /tmp/commit_msg.txt &&
+          touch /tmp/commit_msg.txt &&
+          echo 'automatic sync at '$(date +%Y.%m.%d)' '$(date +%H:%M:%S)' by '$(git config user.name) >> /tmp/commit_msg.txt &&
+          echo ' DELETED: '$GIT_DELETED >> /tmp/commit_msg.txt &&
+          echo ' MODIFIED: '$GIT_MODIFIED >> /tmp/commit_msg.txt &&
+          echo ' ADDED: '$GIT_OTHERS >> /tmp/commit_msg.txt &&
+          git add -A &&
+          git commit -F /tmp/commit_msg.txt &&
+          rm /tmp/commit_msg.txt
+      fi
       git pull &&
-      git push -u origin HEAD
+        git push -u origin HEAD
+    fi
+  }
+
+  # [RUN]
+  eval SEARCH_PATH=$1
+  DIRS=$(find $SEARCH_PATH -maxdepth 1 ! -path $SEARCH_PATH -type d)
+  if [ -d "$SEARCH_PATH/.git" ]; then
+    while true; do
+      echo
+      echo "You have a top level .git repo in $SEARCH_PATH !"
+      read -p "Do you wish that i try to sync in the subfolders first? [y|n] " yn
+      case $yn in
+        [Yy]* )
+          for D in $DIRS
+          do
+            if [[ ! $D == *".git"* ]]; then
+              syncGit $D
+            fi
+          done
+          break;;
+        [Nn]* )
+          break;;
+        * ) echo "Please answer yes or no.";;
+      esac
+    done
+    syncGit $SEARCH_PATH
   fi
-}
 
-# [RUN]
-eval SEARCH_PATH=$1
-DIRS=$(find $SEARCH_PATH -maxdepth 1 ! -path $SEARCH_PATH -type d)
-if [ -d "$SEARCH_PATH/.git" ]; then
-  while true; do
-    echo
-    echo "You have a top level .git repo in $SEARCH_PATH !"
-    read -p "Do you wish that i try to sync in the subfolders first? [y|n] " yn
-    case $yn in
-      [Yy]* )
-        for D in $DIRS
-        do
-          if [[ ! $D == *".git"* ]]; then
-            syncGit $D
-          fi
-        done
-        break;;
-      [Nn]* )
-        break;;
-      * ) echo "Please answer yes or no.";;
-    esac
-  done
-  syncGit $SEARCH_PATH
-fi
-
-# [END]
-cd $CURRENT_WORKING_DIR
+  # [END]
+  cd $CURRENT_WORKING_DIR
